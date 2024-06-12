d33e4b4b1be4c9a0824a1214e274407c77b3624b
faizyabkhangithub <158143998+faizyabkhangithub@users.noreply.github.com>
2024-06-12 11:08:15 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import Loginsignup from './component/Loginsignup/Loginsignup';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+         <Loginsignup/>
+    </div>
+  );
+}
+
+export default App;
